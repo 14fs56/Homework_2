74b7f15c0b37e0b7f28a9f9ca4d6a98097e3ab1d
Home PC <Home PC@DESKTOP-6JEL7L0>
2023-07-26 19:38:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Я - юниор! =)");
+            Console.ReadKey();
+        }
+    }
+}
